09ca10aba2e7fc6784babd7f307ccbb89a384801
jhmk <jhmk@users.noreply.github.com>
2017-06-30 11:07:13 +0200

    Create route-add.sh
    make VPN the default gateway and added exceptions

---
+++ route-add.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ 
+sudo route add default dev tunVPN
+sudo route add -host *URL ot the DB Server* dev eth0
+sudo route add -host *linux update server of your distro* dev eth0
+sudo route add -host resolver1.opendns.com dev tunVPN
+sudo route add -host resolver3.opendns.com dev eth0
+sudo route add -host www.whatismyip.com tunVPN
