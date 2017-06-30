d32d5106a8109b0e1c99a4285618b3a132a37c07
jhmk <jhmk@users.noreply.github.com>
2017-06-30 11:32:33 +0200

    Update sendmail.sh
    Edit Key, ID & Mail address

--- sendmail.sh
+++ sendmail.sh
@@ -2,6 +2,6 @@
 curl -s --user 'api:key-*YOURKEYID*' \
     https://api.mailgun.net/v3/sandbox*ID*.mailgun.org/messages \
     -F from='VPN Alert <mailgun@sandbox*ID*.mailgun.org>' \
-    -F to=*YOUR MAIL ADDRESS \
+    -F to=*YOUR MAIL ADDRESS* \
     -F subject='VPN on Proxy 1 ' \
     -F text='Looks like VPN is down and please restart it'
