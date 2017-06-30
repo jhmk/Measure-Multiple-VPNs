69d919e111e82fe9261b0cd539254166e98b8f63
jhmk <jhmk@users.noreply.github.com>
2017-06-30 11:35:13 +0200

    Update check_vpn.sh

--- check_vpn.sh
+++ check_vpn.sh
@@ -4,7 +4,7 @@ public_ip=$(curl --interface eth0 ident.me --max-time 120)
 echo $current_external_ip
 echo $public_ip
 if [ $current_external_ip = $public_ip ]; then
-	sudo sh /home/ubuntu/sendmail.sh
+	sudo sh /home/ubuntu/sendmail.sh && sudo sh /home/ubuntu/send-telegram.sh
 else
 	echo "VPN is up and running"
 fi
